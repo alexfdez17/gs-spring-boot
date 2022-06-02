1028a1b8edb26382c39d9d904611b667b3c6da93
alexfdez17 <alexfdez17@gmail.com>
2022-06-02 11:15:37 +0200

    test commit

--- initial/src/main/java/com/example/springboot/HelloController.java
+++ initial/src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot! Edited";
+		return "Greetings from Spring Boot! Editedx";
 	}
 
 }
